8f9df755c1136a4db98193b47a9ff3a4bc25df16
Arachild <49368978+Arachild@users.noreply.github.com>
2019-04-20 12:57:04 +0800

    new
    add some animated

--- js/fixbar.js
+++ js/fixbar.js
@@ -52,4 +52,26 @@ function myFunction() {
                     });
                 }
             }
-        });
+});
+            //ANIMATED
+
+            $('.js--wp-1').waypoint(function(direction){
+                $('.js--wp-1').addClass('animated bounceInLeft');
+            },{
+                offset:'50%'
+            })
+
+            $('.js--wp-2').waypoint(function(direction){
+                $('.js--wp-2').addClass('animated bounceInRight');
+            },{
+                offset:'50%'
+            })
+
+            $('.js--wp-3').waypoint(function(direction){
+                $('.js--wp-3').addClass('animated fadeIn');
+            },{
+                offset:'50%'
+            });
+
+
+        
